cfab0d8bb9fad0cc6afa2e99c22e1c06fba2b286
Luis Castillo <raul@luiss-mbp.myfiosgateway.com>
2020-12-28 22:11:09 -0500

    commit tree 48603817b5f6dbb6b8826dcd21377fe310a603e0

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,23 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :word
+    
+    def initialize(word)
+        @word = word
+    end
+
+    def match(string)
+        string.find_all do |a|
+            a.split("").sort == word.split("").sort
+        end
+    end      
+end
+
+# Anagram
+#   should detect no matches
+#   should detect a simple anagram
+#   should detect an anagram
+#   should detect multiple anagrams
+
+# Finished in 0.0071 seconds (files took 0.15958 seconds to load)
+# 4 examples, 0 failures
